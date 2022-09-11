65c7f8e0829911bb066d0fd11b8e4f6dc775d456
witer33 <>
2022-09-11 14:11:04 +0200

    Added PyPi setup.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bladeorm',
+    version='0.0.1',
+    license='MIT',
+    author="Witer33",
+    author_email='dev@witer33.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/witer33/bladeorm',
+    keywords='postgres psql orm',
+    install_requires=[
+          'asyncpg',
+      ],
+
+)
